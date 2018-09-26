f98e7134f9f01cafb3d97bfc3692ed897ae4e233
Tobias.Hastings <tobias.hastings@ft.com>
2018-09-26 08:56:43 +0100

    caller 1

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ var express = require('express');
 const app = express();
 var router = express.Router();
 const { apiAuthMiddleware: requireApiKey } = require('./auth');
+const fetch = require('node-fetch');
 
 router.get('/testEndpoint', requireApiKey,(req,res) => {
   res.send("hello");
+});
+
+router.get('callthtwitter/:screenname',requireApiKey,(req,res) => {
+	const screenname = req.params.screenname.toLowerCase();
+	console.log('screenname: '+screenname);
+	res.sent("received");
 });
 
 
